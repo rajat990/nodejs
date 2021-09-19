1afd1d6658d1069b2485d4b1af32f1055cd76dac
rajat990 <50400854+rajat990@users.noreply.github.com>
2021-09-19 23:08:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const db_ip = process.env.DB_HOST
 const db_port = process.env.DB_PORT
 const db_collection_name = process.env.DB_COLLECTION_NAME
 const url = `mongodb://${db_ip}:${db_port}/${db_collection_name}`
-
-
+<!-- //dsdfd -->
 mongoose.connect(url,
     err => {
         if (err) throw err;
